d5d86db3cd7de344aab307ea560a59fd7473bd3b
snehanaik111 <snehanaik1117@gmail.com>
2024-05-22 14:27:32 +0530

    Update app.py json2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,render_template, redirect,session,url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
-
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
